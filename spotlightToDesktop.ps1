bc9718f4182c7dd58e7123ffcaed2a7dfc75e220
pwstegman <pwstegman@gmail.com>
2016-09-03 19:23:22 -0500

    More reliably live updates wallpaper
    Fixes the problem where the wallpaper was changed in registry, but the wallpaper image wasn't immediately reloaded

--- spotlightToDesktop.ps1
+++ spotlightToDesktop.ps1
@@ -1,13 +1,31 @@
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+namespace Wallpaper{
+    public class Setter {
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+        public static void SetWallpaper ( string path ) {
+            SystemParametersInfo( 20, 0, path, 0x01 | 0x02 );
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
+            key.SetValue(@"WallpaperStyle", "10");
+            key.SetValue(@"TileWallpaper", "0");
+            key.Close();
+        }
+    }
+}
+"@
+#Code for the namespace Wallpaper modified from http://poshcode.org/491
+
 $key = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lock Screen\Creative'
 $picture = (Get-ItemProperty -Path $key -Name LandscapeAssetPath).LandscapeAssetPath
-$date = Get-Date -UFormat "%Y%m%d"
-$jpg = "$date.jpg"
-$savePath = "C:\Users\[username]\Pictures\Spotlight\$jpg"
+$name = Split-Path -Path $picture -Leaf
+$jpg = "$name.jpg"
+$savePath = "C:\Users\[username]]\Pictures\Spotlight\$jpg"
 if(!(Test-Path $savePath)){
     cp $picture $savePath
-    Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $savePath
-    rundll32.exe user32.dll, UpdatePerUserSystemParameters
-    echo "Set desktop background"
-}else{
-    echo "Desktop background already set"
+    echo "Copied over new Spotlight image"
 }
+[Wallpaper.Setter]::SetWallpaper( $savePath )
+echo "Set desktop background"
